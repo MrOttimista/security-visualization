a3e182518a003b52f242d83f6d6ad67758d6a57c
mrottimista <adel.mohamed855@gmail.com>
2021-06-12 00:42:35 +0200

    create routes for the components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import Home from './components';
+import Home from "./components";
+import CipherFunctions from "./components/CipherFunctions";
+import BlockChain from "./components/BlockChain";
+import RSA from "./components/RSA";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-
           <Switch>
-      
+            <Route path="/blockChain">
+              <BlockChain />
+            </Route>
+            <Route path="/cipherFunctions">
+              <CipherFunctions />
+            </Route>
+            <Route path="/rsa">
+              <RSA />
+            </Route>
             <Route path="/">
-             <Home />
+              <Home />
             </Route>
           </Switch>
-
         </div>
       </Router>
     </div>
