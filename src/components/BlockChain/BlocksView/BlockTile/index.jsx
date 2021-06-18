2f6a4b456a2d0943a9eca0cbc4340379409db31b
KarimElghamry <Karimelghamry@gmail.com>
2021-06-18 13:32:43 +0200

    add: BlockTile to BlocksView

---
+++ src/components/BlockChain/BlocksView/BlockTile/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './BlockTile.module.css';
+
+const BlockTile = () => {
+  return <div></div>;
+};
+
+export default BlockTile;
--- src/components/BlockChain/BlocksView/index.jsx
+++ src/components/BlockChain/BlocksView/index.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import styles from './BlocksView.module.css';
+import BlockTile from './BlockTile';
+
+const blocks = ['', '', ''];
 
 const BlocksView = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      {blocks.map((block, index) => (
+        <BlockTile></BlockTile>
+      ))}
+    </div>
+  );
 };
 
 export default BlocksView;
