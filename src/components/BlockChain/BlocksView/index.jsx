4b4506d1271771b9388368cb291d2e2895bab934
KarimElghamry <Karimelghamry@gmail.com>
2021-06-18 15:21:33 +0200

    add: scroll side effect to BlocksView

--- src/components/BlockChain/BlocksView/index.jsx
+++ src/components/BlockChain/BlocksView/index.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import styles from './BlocksView.module.css';
 import BlockTile from './BlockTile';
 
 const BlocksView = ({blocks = []}) => {
+  const containerRef = useRef(null);
+
+  // side effect for scrolling down after blcok addition
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    containerRef.current.scrollTop = containerRef.current.scrollHeight;
+  }, [blocks, containerRef]);
+
   /* TODO: change key to block hash */
 
   return (
-    <div className={styles.container}>
+    <div ref={containerRef} className={styles.container}>
       {blocks.map((block, index) => (
         <React.Fragment key={index}>
           <BlockTile {...block}></BlockTile>
