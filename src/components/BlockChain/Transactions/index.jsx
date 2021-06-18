c7fdd416a60a95425a073e4c95ce6a88150a606e
KarimElghamry <Karimelghamry@gmail.com>
2021-06-18 13:19:17 +0200

    add: container styling to transactions

--- src/components/BlockChain/Transactions/index.jsx
+++ src/components/BlockChain/Transactions/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './transactions.module.css';
 
 const Transactions = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <h1>Unconfirmed Transactions</h1>
+    </div>
+  );
 };
 
 export default Transactions;
