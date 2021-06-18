ecd8a735dcfac2038d9cd37dd1663bed4f922748
KarimElghamry <Karimelghamry@gmail.com>
2021-06-18 15:38:29 +0200

    add: TransactionTile to Transactions

--- src/components/BlockChain/Transactions/index.jsx
+++ src/components/BlockChain/Transactions/index.jsx
@@ -1,12 +1,21 @@
 import {Button, Row} from 'antd';
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './transactions.module.css';
+import TransactionTile from './TransactionTile';
 
 const Transactions = ({addBlock = () => {}}) => {
+  const [transactionsList, setTransactionsList] = useState([]);
+
   return (
     <div className={styles.container}>
       <h1>Transactions</h1>
-      <div className={styles['transactions-list']}></div>
+      <div className={styles['transactions-list']}>
+        {transactionsList.map((transaction, index) => (
+          <React.Fragment key={index}>
+            <TransactionTile transaction={transaction}></TransactionTile>
+          </React.Fragment>
+        ))}
+      </div>
       <Row>
         <Button style={{margin: '10px'}}>Add transaction</Button>
         <Button
