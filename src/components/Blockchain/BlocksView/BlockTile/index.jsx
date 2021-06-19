17c3544b2ec6c428740fe0d591c23182b78ccc93
KarimElghamry <Karimelghamry@gmail.com>
2021-06-19 19:58:55 +0200

    add: modal to blocktile

--- src/components/Blockchain/BlocksView/BlockTile/index.jsx
+++ src/components/Blockchain/BlocksView/BlockTile/index.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './BlockTile.module.css';
 import GroundSign from './GroundSign';
 import UpsideArrow from './UpsideArrow';
+import {Modal} from 'antd';
 
 const BlockTile = ({prevHash, timeStamp, hash, transactionsList, nonce}) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
     <div className={styles.container}>
       {prevHash ? <UpsideArrow></UpsideArrow> : <GroundSign></GroundSign>}
       <div className={styles.block}>
         <p>prevHash: {prevHash ?? 'none'}</p>
         <p>timeStamp: {timeStamp}</p>
-        <p>transactions</p>
+        <p className={styles.link} onClick={() => setIsModalVisible(true)}>
+          Transactions (click to show)
+        </p>
         <p>nonce: {nonce}</p>
         <p>hash: {hash}</p>
       </div>
+      <Modal
+        visible={isModalVisible}
+        onOk={() => setIsModalVisible(false)}
+        onCancel={() => setIsModalVisible(false)}
+        cancelText
+      ></Modal>
     </div>
   );
 };
