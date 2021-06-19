4e28e9f6d570e85a5f5bd99f1baede746023b3d0
Ali-Amin <ali.m.amin98@gmail.com>
2021-06-19 15:41:30 +0200

    add: transaction input form

--- src/components/BlockChain/Transactions/TransactionTile/index.jsx
+++ src/components/BlockChain/Transactions/TransactionTile/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import styles from './TransactionTile.module.css';
+import { Form, Input, InputNumber } from "antd";
 
-const TransactionTile = ({transaction = {}}) => {
-  return <div className={styles.container}></div>;
+
+const TransactionTile = ({ transaction = {} }) => {
+
+  return <div className={styles.container}>
+    <Form labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+
+      <Form.Item label="From" required={true} style={{ margin: 4 }}>
+        <Input type="text"></Input>
+      </Form.Item>
+      <Form.Item label="To" required={true} style={{ margin: 4 }}>
+        <Input type="text"></Input>
+      </Form.Item>
+      <Form.Item label="Amount" required={true} style={{ margin: 4 }}>
+        <InputNumber type="number" style={{ width: "100%" }}></InputNumber>
+      </Form.Item>
+
+    </Form>
+  </div >;
 };
 
 export default TransactionTile;
